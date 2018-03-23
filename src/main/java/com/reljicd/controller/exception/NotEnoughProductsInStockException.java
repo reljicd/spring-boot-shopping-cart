acbd6cc8362da410595a8ed947e0616e52f86243
Dušan Reljić <reljicd@gmail.com>
2018-03-23 11:34:22 +0100

    deleted unused field

--- src/main/java/com/reljicd/controller/exception/NotEnoughProductsInStockException.java
+++ src/main/java/com/reljicd/controller/exception/NotEnoughProductsInStockException.java
@@ -2,12 +2,8 @@ package com.reljicd.controller.exception;
 
 import com.reljicd.model.Product;
 
-/**
- * Created by reljicd on 2/2/17.
- */
 public class NotEnoughProductsInStockException extends Exception {
 
-    private Product product;
     private static final String DEFAULT_MESSAGE = "Not enough products in stock";
 
     public NotEnoughProductsInStockException() {
@@ -16,8 +12,6 @@ public class NotEnoughProductsInStockException extends Exception {
 
     public NotEnoughProductsInStockException(Product product) {
         super(String.format("Not enough %s products in stock. Only %d left", product.getName(), product.getQuantity()));
-
-        this.product = product;
     }
 
 }
