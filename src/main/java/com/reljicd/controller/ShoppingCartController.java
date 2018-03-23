5a98179a072112ea1d83fdfb8aca021d69037b42
Dušan Reljić <reljicd@gmail.com>
2018-03-23 11:50:51 +0100

    cleanup and using Optionals

--- src/main/java/com/reljicd/controller/ShoppingCartController.java
+++ src/main/java/com/reljicd/controller/ShoppingCartController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Created by Dusan on 21-May-17.
- */
 @Controller
 public class ShoppingCartController {
 
@@ -36,13 +33,13 @@ public class ShoppingCartController {
 
     @GetMapping("/shoppingCart/addProduct/{productId}")
     public ModelAndView addProductToCart(@PathVariable("productId") Long productId) {
-        shoppingCartService.addProduct(productService.findById(productId));
+        productService.findById(productId).ifPresent(shoppingCartService::addProduct);
         return shoppingCart();
     }
 
     @GetMapping("/shoppingCart/removeProduct/{productId}")
     public ModelAndView removeProductFromCart(@PathVariable("productId") Long productId) {
-        shoppingCartService.removeProduct(productService.findById(productId));
+        productService.findById(productId).ifPresent(shoppingCartService::removeProduct);
         return shoppingCart();
     }
 
