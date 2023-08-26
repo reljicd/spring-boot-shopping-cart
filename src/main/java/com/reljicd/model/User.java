89d9119ab0ee70c493ebf8600f5ee76a7224d56a
prafitradimas <prftrdms@gmail.com>
2023-08-27 01:29:51 +0700

    feat: add UserDetailsService and modify User's fetch to EAGER

--- src/main/java/com/reljicd/model/User.java
+++ src/main/java/com/reljicd/model/User.java
@@ -45,7 +45,7 @@ public class User {
     @Column(name = "active", nullable = false)
     private int active;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Collection<Role> roles;
 
